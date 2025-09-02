0991906ffb2db378457937e584f4a29f0728436f
Mohamad Kanafani <mkanafani40@gmail.com>
2025-09-03 01:36:44 +0300

    Added Final Changes

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,20 @@ import authRoutes from "./routes/auth.route.js"
 import movieRoutes from "./routes/movie.route.js"
 import tvRoutes from "./routes/tv.route.js"
 import searchRoutes from "./routes/search.route.js"
+import path from "path"
 import { ENV_VARS } from "./config/envVars.js"
 import { connectDB } from "./config/db.js"
 import cookieParser from "cookie-parser"
 import protectRoute from "./middlewares/protectRoute.js"
 const app = express ()
+
+
+const __dirname = path.resolve()
+
 app.use(express.json())
 app.use(cookieParser())
+
+
 
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/movie",protectRoute, movieRoutes)
@@ -18,7 +25,12 @@ app.use("/api/v1/search",protectRoute, searchRoutes)
 
 connectDB()
 const port = ENV_VARS.PORT
-
+if(ENV_VARS.NODE_DEV ==="production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
+    app.get("*", (req,res)=>{
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
 app.listen(port,()=>{
     console.log(`server started at ${port} `)
 })
